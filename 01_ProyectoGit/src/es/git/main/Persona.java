d39bae4fbccbfe618277d90243e1e00e5314ab2c
Your Name <mikelballes1@gmail.com>
2023-06-04 00:18:23 +0200

    Crear la clase persona2

--- 01_ProyectoGit/src/es/git/main/Persona.java
+++ 01_ProyectoGit/src/es/git/main/Persona.java
@@ -1,5 +1,19 @@
 package es.git.main;
 
 public class Persona {
+private String Nombre;
+private int Edad;
+public String getNombre() {
+	return Nombre;
+}
+public void setNombre(String nombre) {
+	Nombre = nombre;
+}
+public int getEdad() {
+	return Edad;
+}
+public void setEdad(int edad) {
+	Edad = edad;
+}
 
 }
